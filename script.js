bc37762b28dc97b189b08561a96ba1ffca7129bc
topvirtualbox <topvirtualbox@gmail.com>
2021-05-20 22:45:04 -0300

    Add grid hover effect

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ const size = 15;
 
 window.addEventListener("load", createGrid(size));
 
+let grid = document.querySelectorAll(".screen>div");
+
+for (let i = 0; i < grid.length; i++) {
+    grid[i].addEventListener("mouseover", function() {
+        grid[i].setAttribute("style", "background-color: #000");
+    });
+}
+
 function createGrid(num) {
     screen.style.gridTemplateRows = "repeat(" + num + ", auto)";
     screen.style.gridTemplateColumns = "repeat(" + num + ", auto)";
