d031242dfd64c653d30123b2adcfcdb172b211e8
topvirtualbox <topvirtualbox@gmail.com>
2021-05-23 11:46:55 -0300

    Rework script.js for better readability

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
-const screen = document.querySelector(".screen");
-const clear = document.querySelector("#clear");
 const black = document.querySelector("#black");
 const rgb = document.querySelector("#rgb");
-const size = 15;
-window.addEventListener("load", createGrid(size));
-let mode = "black";
-let grid = document.querySelectorAll(".screen>div");
+const clear = document.querySelector("#clear");
+let color = "black";
 
-for (let i = 0; i < grid.length; i++) {
-    grid[i].addEventListener("mouseover", function() {
-        if (mode === "black") {
-            grid[i].setAttribute("style", "background-color: #000");
-        } else {
-            grid[i].setAttribute("style", "background-color: #" + Math.floor(Math.random()*16777216).toString(16));
-        }
-    });
-}
+window.addEventListener("load", createGrid(16));
+window.addEventListener("load", paintGrid());
 
+black.addEventListener("click", function() {
+    color = "black";
+});
+rgb.addEventListener("click", function() {
+    color = "rgb";
+});
 clear.addEventListener("click", function() {
     clearGrid();
 });
-black.addEventListener("click", function() {
-    mode = "black";
-});
-rgb.addEventListener("click", function() {
-    mode = "rgb";
-});
 
 function createGrid(num) {
+    let screen = document.querySelector(".screen");
     screen.style.gridTemplateRows = "repeat(" + num + ", auto)";
     screen.style.gridTemplateColumns = "repeat(" + num + ", auto)";
     screen.style.gridGap = "1px 1px";
     for (let i = 0; i < num ** 2; i++) {
         let div = document.createElement("div");
-        screen.appendChild(div).setAttribute("style", "background-color: #fff");
-    }
+        screen.appendChild(div).style.backgroundColor = "#fff";
+    } 
 }
 
 function clearGrid() {
+    let grid = document.querySelectorAll(".screen>div");
     for (let i = 0; i < grid.length; i++) {
-        grid[i].setAttribute("style", "background-color: #fff");
+        grid[i].style.backgroundColor = "#fff";
     }
 }
 
+function paintGrid() {
+    let grid = document.querySelectorAll(".screen>div");
+    for (let i = 0; i < grid.length; i++) {
+        grid[i].addEventListener("mouseover", function() {
+            if (color === "black") {
+                grid[i].style.backgroundColor = "#000";
+            } else {
+                grid[i].style.backgroundColor = "#" + Math.floor(Math.random()*16777216).toString(16);
+            }
+        });
+    }
+}
