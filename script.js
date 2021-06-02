2a398eabe63b8e28bf79d0db171d82a5b99e2402
topvirtualbox <topvirtualbox@gmail.com>
2021-06-01 21:28:20 -0300

    Add grid button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const mode = document.querySelector("#mode");
+const grid = document.querySelector("#grid");
 const clear = document.querySelector("#clear");
 const size = document.querySelector("#size");
+const mode = document.querySelector("#mode");
 const black = document.querySelector("#black");
 const white = document.querySelector("#white");
 const red = document.querySelector("#red");
@@ -8,145 +9,168 @@ const green = document.querySelector("#green");
 const blue = document.querySelector("#blue");
 const yellow = document.querySelector("#yellow");
 const rgb = document.querySelector("#rgb");
-let theme = "dark";
-let color = "black";
 
-window.addEventListener("load", createGrid(16));
-window.addEventListener("load", paintGrid());
+let isColor = "black";
+let isGrid = "on";
+let isMode = "dark";
 
-mode.addEventListener("click", function() {
-    let root = document.querySelector(":root");
-    if (theme === "dark") {
-        root.style.setProperty("--color", "#111");
-        root.style.setProperty("--background", "#eee");
-        root.style.setProperty("--border", "#fff");
-        theme = "light";
-    } else if (theme === "light") {
-        root.style.setProperty("--color", "#eee");
-        root.style.setProperty("--background", "#111");
-        root.style.setProperty("--border", "#000");
-        theme = "dark";
-    }
+window.addEventListener("load", createScreen(15));
+
+grid.addEventListener("click", function() {
+    changeGrid();
 });
 clear.addEventListener("click", function() {
-    clearGrid();
+    clearScreen();
 });
 size.addEventListener("click", function() {
-    sizeGrid();
+    changeSize();
+});
+mode.addEventListener("click", function() {
+    changeMode();
 });
 black.addEventListener("click", function() {
-    color = "black";
+    isColor = "black";
 });
 white.addEventListener("click", function() {
-    color = "white";
+    isColor = "white";
 });
 red.addEventListener("click", function() {
-    color = "red";
+    isColor = "red";
 });
 green.addEventListener("click", function() {
-    color = "green";
+    isColor = "green";
 });
 blue.addEventListener("click", function() {
-    color = "blue";
+    isColor = "blue";
 });
 yellow.addEventListener("click", function() {
-    color = "yellow";
+    isColor = "yellow";
 });
 rgb.addEventListener("click", function() {
-    color = "rgb";
+    isColor = "rgb";
 });
 
-function createGrid(num) {
+function createScreen(num) {
+    removeScreen();
     let screen = document.querySelector(".screen");
     screen.style.gridTemplateRows = "repeat(" + num + ", auto)";
     screen.style.gridTemplateColumns = "repeat(" + num + ", auto)";
-    screen.style.gridGap = "1px 1px";
+    if (isGrid === "on") {
+        screen.style.gridGap = "1px 1px";
+    } else if (isGrid === "off") {
+        screen.style.gridGap = "0px 0px";
+    }
     for (let i = 0; i < num ** 2; i++) {
         let div = document.createElement("div");
         div.style.userSelect = "none";
         screen.appendChild(div).style.backgroundColor = "var(--white)";
-    } 
+    }
+    paintScreen();
 }
 
-function clearGrid() {
-    let grid = document.querySelectorAll(".screen>div");
-    for (let i = 0; i < grid.length; i++) {
-        grid[i].style.backgroundColor = "var(--white)";
+function removeScreen() {
+    let divs = document.querySelectorAll(".screen>div");
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].remove();
     }
 }
 
-function paintGrid() {
-    let grid = document.querySelectorAll(".screen>div");
-    for (let i = 0; i < grid.length; i++) {
-        grid[i].addEventListener("mousedown", function() {
-            switch (color) {
+function paintScreen() {
+    let divs = document.querySelectorAll(".screen>div");
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("mousedown", function() {
+            switch (isColor) {
                 case "black":
-                    grid[i].style.backgroundColor = "var(--black)";
+                    divs[i].style.backgroundColor = "var(--black)";
                     break;
                 case "white":
-                    grid[i].style.backgroundColor = "var(--white)";
+                    divs[i].style.backgroundColor = "var(--white)";
                     break;
                 case "red":
-                    grid[i].style.backgroundColor = "var(--red)";
+                    divs[i].style.backgroundColor = "var(--red)";
                     break;
                 case "green":
-                    grid[i].style.backgroundColor = "var(--green)";
+                    divs[i].style.backgroundColor = "var(--green)";
                     break;
                 case "blue":
-                    grid[i].style.backgroundColor = "var(--blue)";
+                    divs[i].style.backgroundColor = "var(--blue)";
                     break;
                 case "yellow":
-                    grid[i].style.backgroundColor = "var(--yellow)";
+                    divs[i].style.backgroundColor = "var(--yellow)";
                     break;
                 case "rgb":
-                    grid[i].style.backgroundColor = "#" + Math.floor(Math.random()*16777216).toString(16);
+                    divs[i].style.backgroundColor = "#" + Math.floor(Math.random()*16777216).toString(16);
             }
         });
-        grid[i].addEventListener("mouseover", function(e) {
+        divs[i].addEventListener("mouseover", function(e) {
             if (e.buttons === 1) {
-                switch (color) {
+                switch (isColor) {
                     case "black":
-                        grid[i].style.backgroundColor = "var(--black)";
+                        divs[i].style.backgroundColor = "var(--black)";
                         break;
                     case "white":
-                        grid[i].style.backgroundColor = "var(--white)";
+                        divs[i].style.backgroundColor = "var(--white)";
                         break;
                     case "red":
-                        grid[i].style.backgroundColor = "var(--red)";
+                        divs[i].style.backgroundColor = "var(--red)";
                         break;
                     case "green":
-                        grid[i].style.backgroundColor = "var(--green)";
+                        divs[i].style.backgroundColor = "var(--green)";
                         break;
                     case "blue":
-                        grid[i].style.backgroundColor = "var(--blue)";
+                        divs[i].style.backgroundColor = "var(--blue)";
                         break;
                     case "yellow":
-                        grid[i].style.backgroundColor = "var(--yellow)";
+                        divs[i].style.backgroundColor = "var(--yellow)";
                         break;
                     case "rgb":
-                        grid[i].style.backgroundColor = "#" + Math.floor(Math.random()*16777216).toString(16);
+                        divs[i].style.backgroundColor = "#" + Math.floor(Math.random()*16777216).toString(16);
                 }
             }
         });
     }
 }
 
-function removeGrid() {
-    let grid = document.querySelectorAll(".screen>div");
-    for (let i = 0; i < grid.length; i++) {
-        grid[i].remove();
+function changeGrid() {
+    let screen = document.querySelector(".screen");
+    if (isGrid === "on") {
+        screen.style.gridGap = "0px 0px";
+        isGrid = "off";
+    } else if (isGrid === "off") {
+        screen.style.gridGap = "1px 1px";
+        isGrid = "on";
     }
 }
 
-function sizeGrid() {
+function clearScreen() {
+    let divs = document.querySelectorAll(".screen>div");
+    for (let i = 0; i < divs.length; i++) {
+        divs[i].style.backgroundColor = "var(--white)";
+    }
+}
+
+function changeSize() {
     let answer = prompt("Size:");
     while (answer !== null && (isNaN(answer) || answer < 1 || answer > 64 || answer % 1 !== 0)) {
         answer = prompt("Insert a number between 1 and 64:");
     }
     if (answer !== null) {
         answer = Math.floor(answer);
-        removeGrid();
-        createGrid(answer);
-        paintGrid();
+        createScreen(answer);
+    }
+}
+
+function changeMode() {
+    let root = document.querySelector(":root");
+    if (isMode === "dark") {
+        root.style.setProperty("--color", "#111");
+        root.style.setProperty("--background", "#eee");
+        root.style.setProperty("--border", "#fff");
+        isMode = "light";
+    } else if (isMode === "light") {
+        root.style.setProperty("--color", "#eee");
+        root.style.setProperty("--background", "#111");
+        root.style.setProperty("--border", "#000");
+        isMode = "dark";
     }
 }
